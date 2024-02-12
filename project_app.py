d07b51f995c962fee099b03dfeed836fe8e57c96
Kelvin Rotich <141912522+Kelvin-Rotich824@users.noreply.github.com>
2024-02-12 11:42:24 +0300

    Updated files

--- project_app.py
+++ project_app.py
@@ -201,38 +201,37 @@ ax.set_ylabel("Frequency")
 st.pyplot(fig)
 
 # Prophet Forecast
-# Copying the dataframe.
-ts = data.copy()
-# Setting 'year' as the index
-ts['year'] = pd.to_datetime(ts['year'], format='%Y')
-# Dropping the null values
-ts = ts.dropna(subset=['dry weight loss'])
-# Grouping the dataframe
-ts = ts.groupby('year').aggregate({'dry weight loss':'mean'})
-# Resampling the data to daily
-ts = ts.resample('D').asfreq()
-# Filling the null values
-ts = ts.interpolate(method='linear', axis=0, limit_direction='forward')
-# Resetting the index and renaming the columns
-ts_prophet = ts.reset_index()
-ts_prophet = ts_prophet.rename(columns={'year': 'ds', 'dry weight loss': 'y'})
-    
+
 # Create a Prophet model
 ts_model = joblib.load('ts_model.pkl')
 
-# Fit the model to your data
-ts_model.fit(ts_prophet)
-
 # Define a function to make predictions
 def make_prediction(date):
-  future_data = ts_model.make_future_dataframe(periods=18263, freq=D, include_history=True)
-  forecast = ts_model.predict(future_data)
-  forecast = pd.DataFrame({
+    # Copying the dataframe.
+    ts = data.copy()
+    # Setting 'year' as the index
+    ts['year'] = pd.to_datetime(ts['year'], format='%Y')
+    # Dropping the null values
+    ts = ts.dropna(subset=['dry weight loss'])
+    # Grouping the dataframe
+    ts = ts.groupby('year').aggregate({'dry weight loss':'mean'})
+    # Resampling the data to daily
+    ts = ts.resample('D').asfreq()
+    # Filling the null values
+    ts = ts.interpolate(method='linear', axis=0, limit_direction='forward')
+    # Resetting the index and renaming the columns
+    ts_prophet = ts.reset_index()
+    ts_prophet = ts_prophet.rename(columns={'year': 'ds', 'dry weight loss': 'y'})
+    # Fit the model to your data
+    ts_model.fit(ts_prophet)
+    future_data = ts_model.make_future_dataframe(periods=18263, freq=D, include_history=True)
+    forecast = ts_model.predict(future_data)
+    forecast = pd.DataFrame({
         "ds": forecast["ds"],
         "yhat": forecast["yhat"],
         "yhat_lower": forecast["yhat_lower"],
         "yhat_upper": forecast["yhat_upper"]})
-  return prediction
+    return prediction
 
 # Streamlit interface
 st.header("Time Series Forecast")
